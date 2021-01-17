fdefca957d1825687b356c32db88155f0d7c3148
david <kingbestd030@gmail.com>
2021-01-17 15:37:11 +0100

    fin de la patie add agent, activation et des activation

--- src/views/UserProfile.jsx
+++ src/views/UserProfile.jsx
@@ -16,14 +16,16 @@ import { Api_route } from '../components/baseApi'
 import axios from 'axios';
 import Spinner from 'react-spinner-material';
 //import Badge from 'react-bootstrap/Badge'
- import 'materialize-css';
+import 'materialize-css';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 
 class UserProfile extends Component {
   constructor() {
     super()
     this.state = {
-      change_panel: true,
+      change_panel: false,
       dataSource: [],
       isload_data: true,
       email: null,
@@ -32,9 +34,9 @@ class UserProfile extends Component {
       phone: null,
       role: "AGENT",
       conf_password: null,
-      modalShow:false,
-      idUser:null,
-      is_createuser:false,
+      modalShow: false,
+      idUser: null,
+      is_createuser: false,
     }
 
 
@@ -64,11 +66,11 @@ class UserProfile extends Component {
         this.setState({
           dataSource: responseJson.content,
           isload_data: false,
-          modalShow:false,
-          idUser:null,
-          is_createuser:false,
+          modalShow: false,
+          idUser: null,
+          is_createuser: false,
         })
-        //console.log(responseJson);
+        console.log(responseJson);
 
       })
       .catch((error) => {
@@ -91,6 +93,7 @@ class UserProfile extends Component {
   }
   submitHandeler = e => {
     e.preventDefault();
+
     const { email, name, password, phone, role, conf_password } = this.state;
     let Body = {
       "email": email,
@@ -100,52 +103,158 @@ class UserProfile extends Component {
       "role": role
 
     };
-    axios.post(Api_route("users?userId=1"), Body)
-      .then(response => {
-        this.Get_user_from_api();
-        console.log(response);
+    if (password == conf_password) {
+      this.setState({
+        is_createuser: true
+      });
+      axios.post(Api_route("users?userId=2"), Body)
+        .then(response => {
+
+          // console.log(response);
+          confirmAlert({
+            title: 'Succés',
+            message: "Enregistré avec succés",
+            buttons: [
+              {
+                label: 'ok',
+                onClick: () => {
+                  this.setState({
+                    email: null,
+                    name: null,
+                    password: null,
+                    phone: null,
+                    role: "AGENT",
+                    conf_password: null,
+                  }, this.Get_user_from_api());
+
+                }
+              }
+            ]
+          })
+
+
+        })
+        .catch(error => {
+          // console.log(error.response.data.message);
+          // alert(error.response.data.message);
+
+          confirmAlert({
+            title: 'Erreur',
+            message: error.response.data.message,
+            buttons: [
+              {
+                label: 'Réessayer',
+                onClick: () => this.setState({
+                  is_createuser: false
+                })
+              }
+            ]
+          })
+
+
+
+        })
+    } else {
+      confirmAlert({
+        title: 'Erreur',
+        message: "Mot de pass non idendique",
+        buttons: [
+          {
+            label: 'Réessayer',
+            onClick: () => this.setState({
+              is_createuser: false
+            })
+          }
+        ]
       })
-      .catch(error => {
-        console.log(error);
-      })
+    }
 
   }
 
-  changeEtatuserHandeler(idUser){
-    if(idUser){
-      axios.patch(Api_route("users/disable-user?updatedBy=1&userId="+idUser))
-      .then(response => {
-        this.Get_user_from_api();
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+  changeEtatuserHandeler(idUser) {
+    if (idUser) {
+      axios.patch(Api_route("users/disable-user?adminId=2&userId=" + idUser))
+        .then(response => {
+
+          console.log(response);
+          confirmAlert({
+            title: 'Succés',
+            message: "Effectué avec succés",
+            buttons: [
+              {
+                label: 'ok',
+                onClick: () => {
+                  this.Get_user_from_api();
+
+                }
+              }
+            ]
+          })
+        })
+        .catch(error => {
+          confirmAlert({
+            title: 'Erreur',
+            message: error.response.data.message,
+            buttons: [
+              {
+                label: 'Réessayer',
+                onClick: () => this.setState({
+                  is_createuser: false
+                })
+              }
+            ]
+          });
+        })
     }
   }
 
-  deleteHandeler(idUser){
-    if(idUser){
-      axios.delete(Api_route("users/"+idUser))
-      .then(response => {
-        this.Get_user_from_api();
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+  deleteHandeler(idUser) {
+    if (idUser) {
+      axios.delete(Api_route("users/" + idUser))
+        .then(response => {
+
+          console.log(response);
+
+          confirmAlert({
+            title: 'Succés',
+            message: "Supprimé avec succés",
+            buttons: [
+              {
+                label: 'ok',
+                onClick: () => {
+                  this.Get_user_from_api();;
+
+                }
+              }
+            ]
+          })
+        })
+        .catch(error => {
+          confirmAlert({
+            title: 'Erreur',
+            message: error.response.data.message,
+            buttons: [
+              {
+                label: 'Réessayer',
+                onClick: () => this.setState({
+                  is_createuser: false
+                })
+              }
+            ]
+          });
+        })
     }
   }
-  modalEvent(event,id){
+  modalEvent(event, id) {
     this.setState({
-      modalShow:event,
-      idUser:id,
+      modalShow: event,
+      idUser: id,
     });
   }
   render() {
     const { email, name, password, phone, role, conf_password } = this.state;
     // console.log(this.state.dataSource);
-    
+
 
     return (
 
@@ -167,12 +276,9 @@ class UserProfile extends Component {
                     title="Ajouter Un utilisateur"
                     content={
 
-                      <Form onSubmit={()=>{
-                        this.setState({
-                          is_createuser:true
-                        },this.submitHandeler);
-                      }}>
-                        
+                      <Form onSubmit={this.submitHandeler}>
+
+
                         <Form.Group controlId="formnom">
                           <Form.Label>Nom complet :</Form.Label>
                           <Form.Control type="text" placeholder="Enter le mon complet" name="name" required value={name} onChange={this.changeHandler} />
@@ -208,26 +314,24 @@ class UserProfile extends Component {
 
                           </Form.Control>
                         </Form.Group>
-                    {
-                      !this.state.is_createuser
-                      ?
-                      <>
-                      <Button variant="primary" type="submit" >
-                      Enregistrer
-                 </Button>
-                    <Button variant="danger" type="button" className="btn_margin" onClick={() => { this.change_panel(false) }}>
-                      Annuler
-                 </Button>
-                 <div className="col-lg-12 bg_danger m-t-3" >
-                   <span>message</span>
-                 </div>
-                 </>
-                 :
-                 <div className="loader">
-                        <Spinner size={120} spinnerColor={"#333"} spinnerWidth={2} visible={this.state.isload_data} />
-                      </div>
+                        {
+                          !this.state.is_createuser
+                            ?
+                            <>
+                              <Button variant="primary" type="submit" >
+                                Enregistrer
+                             </Button>
+                              <Button variant="danger" type="button" className="btn_margin" onClick={() => { this.change_panel(false) }}>
+                                Annuler
+                             </Button>
 
-                    }
+                            </>
+                            :
+                            <div className="loader">
+                              <Spinner size={120} spinnerColor={"#333"} spinnerWidth={2} visible={true} />
+                            </div>
+
+                        }
 
                       </Form>
 
@@ -291,71 +395,87 @@ class UserProfile extends Component {
                             <thead>
                               <tr>
                                 <th >NOM COMPLET</th>
-                                {/* <th >NUMERO DE TELEEPHONE</th> */}
+                                <th >NUMERO DE TELEEPHONE</th>
                                 <th >EMAIL</th>
                                 <th>ROLE</th>
                                 <th>ETAT</th>
                                 <th>DATE DE CREATION</th>
-                                <th>DATE DE MODIFICATION</th>
-                                <th>ACTION</th>
+                                {/* <th>DATE DE MODIFICATION</th> */}
+                                <th></th>
+                                <th></th>
                               </tr>
                             </thead>
 
                             <tbody>
 
-                              {this.state.dataSource.map((data, key) => {
-                                return (
-                                  <tr key={key}>
-                                    <td>{data.name}</td>
-                                    {/* <td>{data.phone}</td> */}
-                                    <td>{data.email}</td>
-                                    <td>{data.role}</td>
-                                    <td>
-                                      {/* <Badge pill variant="primary">
+                              {
+                                this.state.dataSource
+                                  ?
+                                  this.state.dataSource.map((data, key) => {
+                                    return (
+                                      <tr key={key}>
+                                        <td>{data.name}</td>
+                                        <td>{data.phone ? data.phone : "pas de numéro"}</td>
+                                        <td>{data.email}</td>
+                                        <td>{data.role}</td>
+                                        <td>
+                                          {/* <Badge pill variant="primary">
                                         Info
                                       </Badge>{''} */
-                                      }
-                                      <span className={data.enabled?"etat bg_site":"etat bg_danger"}>{data.enabled?"ACTIVÉ":"DESACTIVÉ"} </span>
-                                    </td>
-                                    <td>{data.creationDate}</td>
-                                    <td>{data.updateDate}</td>
-                                   {/* <td><Button variant={data.enabled?"danger":"primary"}>{data.enabled?"ACTIVER":"DESACTIVER"}</Button></td>  */}
-                                   <td> <button className={data.enabled?"bg_danger btn  btn_site":"bg_site btn  btn_site"} onClick={()=>{
-                                     this.setState({
-                                       isload_data:true
-                                     },this.changeEtatuserHandeler(data.id));
-                                   }}  >{data.enabled?"DESACTIVER":"REACTIVER"}</button></td>
-                                   <td>
-                                    {
-                                      this.state.modalShow && this.state.idUser==data.id
-                                      ?
-                                      <div>
-                                      <p>Etez-vous sûr de vouloir supprimer cet agent?</p>
-                                      <p><button className="text-danger" 
-                                      onClick={()=>{
-                                        this.deleteHandeler(data.id);
-                                      }}
-                                      >oui</button>
-                                      <button className="text-primary" 
-                                       onClick={()=>{
-                                        this.modalEvent(false,null);
-                                      }}
-                                      >non</button></p>
+                                          }
+                                          <span className={data.enabled ? "etat bg_site" : "etat bg_danger"}>{data.enabled ? "ACTIVÉ" : "DESACTIVÉ"} </span>
+                                        </td>
+                                        <td>{data.creationDate}</td>
+                                        {/* <td>{data.updateDate}</td> */}
+                                        {/* <td><Button variant={data.enabled?"danger":"primary"}>{data.enabled?"ACTIVER":"DESACTIVER"}</Button></td>  */}
+                                        {
+                                          this.state.modalShow && this.state.idUser == data.id
+                                            ?
+                                            null
+                                            :
+                                            <td> <button className={data.enabled ? "bg_danger btn  btn_site" : "bg_site btn  btn_site"} onClick={() => {
+                                              this.setState({
+                                                isload_data: true
+                                              }, this.changeEtatuserHandeler(data.id));
+                                            }}  >{data.enabled ? "DESACTIVER" : "REACTIVER"}</button></td>
 
-                                    </div>
-                                      :
-                                      <button className="bg_danger btn  btn_site" 
-                                      onClick={()=>{
-                                        this.modalEvent(true,data.id);
-                                      }}
-                                      >suprimer</button>
-                                    
-                                     
-                                    }
-                                    </td>
-                                  </tr>
-                                );
-                              })}
+                                        }
+                                        <td>
+                                          {
+                                            this.state.modalShow && this.state.idUser == data.id
+                                              ?
+                                              <>
+                                                <p>Supprimer cet agent?</p>
+                                                <p><button className="text-danger"
+                                                  onClick={() => {
+                                                    this.setState({
+                                                      isload_data:true
+                                                    },this.deleteHandeler(data.id));
+                                                  }}
+                                                >oui</button>
+                                                  <button className="text-primary"
+                                                    onClick={() => {
+                                                      this.modalEvent(false, null);
+                                                    }}
+                                                  >non</button></p>
+
+                                              </>
+                                              :
+                                              <button className="bg_danger btn  btn_site"
+                                                onClick={() => {
+                                                  this.modalEvent(true, data.id);
+                                                }}
+                                              ><i className="fa fa-trash"></i></button>
+
+
+                                          }
+                                        </td>
+                                      </tr>
+                                    );
+                                  })
+                                  :
+                                  null
+                              }
                             </tbody>
                           </Table>
                           <div className="loader">
